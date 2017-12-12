89fff92e6d262555f8180af2dcb27e9987dbf782
Fumiya Sakai <just1factory@gmail.com>
2017-12-12 17:14:17 +0900

    Initial Commit

---
+++ ReduxSampleSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReduxSampleSwift
+//
+//  Created by 酒井文也 on 2017/12/12.
+//  Copyright © 2017年 酒井文也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
