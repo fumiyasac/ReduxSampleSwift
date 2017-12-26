8fea082ca65d75766edb5a7739a832bf666471ba
Fumiya Sakai <just1factory@gmail.com>
2017-12-26 10:23:10 +0900

    ベース部分をこれから作成

---
+++ ReduxSampleSwift/Redux/State/AppState.swift
@@ -0,0 +1,16 @@
+//
+//  AppState.swift
+//  ReduxSampleSwift
+//
+//  Created by 酒井文也 on 2017/12/17.
+//  Copyright © 2017年 酒井文也. All rights reserved.
+//
+
+import Foundation
+import ReSwift
+
+/*
+struct AppState: ReSwift.StateType {
+    var homeState = HomeState()
+}
+*/
--- ReduxSampleSwift/ViewController/ViewController.swift
+++ ReduxSampleSwift/ViewController/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  ReduxSampleSwift
 //
 //  Created by 酒井文也 on 2017/12/12.
@@ -8,18 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
