52affa48b26b36413067be97b07faf8fab3a9e2f
Fumiya Sakai <just1factory@gmail.com>
2017-12-26 10:23:10 +0900

    ベース部分をこれから作成

---
+++ ReduxSampleSwift/Redux/State/HomeState.swift
@@ -0,0 +1,15 @@
+//
+//  HomeState.swift
+//  ReduxSampleSwift
+//
+//  Created by 酒井文也 on 2017/12/18.
+//  Copyright © 2017年 酒井文也. All rights reserved.
+//
+
+import Foundation
+
+/*
+struct HomeState: StateType {
+    var itemsPerPage: Int = 1
+}
+*/
