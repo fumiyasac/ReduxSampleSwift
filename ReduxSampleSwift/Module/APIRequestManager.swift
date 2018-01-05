13cbddbd6923d36ac1ddeeba81e3005bd907e3d3
Fumiya Sakai <just1factory@gmail.com>
2018-01-06 01:31:00 +0900

    API経由での取得処理の作成

--- ReduxSampleSwift/Module/APIRequestManager.swift
+++ ReduxSampleSwift/Module/APIRequestManager.swift
@@ -8,19 +8,29 @@
 
 import Foundation
 import Alamofire
+import PromiseKit
+import SwiftyJSON
 
 class APIRequestManager {
 
-    private let baseUri = "https://webservice.recruit.co.jp/hotpepper/"
-    private let version = "v1"
+    private static let domain  = "https://webservice.recruit.co.jp/hotpepper/"
+    private static let version = "v1"
+
+    private let key     = Constants.HOTPEPPER_API_KEY
+    private let format  = "json"
 
     //エンドポイントの定義をするenum
     private enum endPoint: String {
         case shop         = "shop"
         case genre        = "genre"
-        case foodCategory = "food_category"
+        case food         = "food"
         case largeArea    = "large_area"
         case middleArea   = "middle_area"
+
+        func getBaseUrl() -> String {
+            let baseUrl = domain + "/\(self.rawValue)/" + version
+            return baseUrl
+        }
     }
 
     //MARK: - Singleton Instance
@@ -32,4 +42,63 @@ class APIRequestManager {
     //MARK: - Functions
 
     //hotpepperのお店IDに紐づくお店情報を取得する
+    func getShopDetailBy(targetId: String) -> Promise<JSON> {
+        let parameters = [
+            "key"    : key,
+            "format" : format,
+            "id"     : targetId,
+        ]
+
+        //Alamofireの非同期通信をPromiseKitの処理でラッピングする
+        return Promise { fulfill, reject in
+            Alamofire.request(endPoint.shop.getBaseUrl(), method: .get, parameters: parameters).validate().responseJSON { response in
+                switch response.result {
+                case .success(let data):
+                    fulfill(JSON(data))
+                case .failure(let error):
+                    reject(error)
+                }
+            }
+        }
+    }
+
+    //hotpepperの料理名一覧を取得する
+    func getFoodList() -> Promise<JSON> {
+        let parameters = [
+            "key"    : key,
+            "format" : format,
+        ]
+
+        //Alamofireの非同期通信をPromiseKitの処理でラッピングする
+        return Promise { fulfill, reject in
+            Alamofire.request(endPoint.food.getBaseUrl(), method: .get, parameters: parameters).validate().responseJSON { response in
+                switch response.result {
+                case .success(let data):
+                    fulfill(JSON(data))
+                case .failure(let error):
+                    reject(error)
+                }
+            }
+        }
+    }
+
+    //hotpepperのジャンル名一覧を取得する
+    func getGenreList() -> Promise<JSON> {
+        let parameters = [
+            "key"    : key,
+            "format" : format,
+        ]
+
+        //Alamofireの非同期通信をPromiseKitの処理でラッピングする
+        return Promise { fulfill, reject in
+            Alamofire.request(endPoint.genre.getBaseUrl(), method: .get, parameters: parameters).validate().responseJSON { response in
+                switch response.result {
+                case .success(let data):
+                    fulfill(JSON(data))
+                case .failure(let error):
+                    reject(error)
+                }
+            }
+        }
+    }
 }
