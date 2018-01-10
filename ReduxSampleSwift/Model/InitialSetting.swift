05d54363034ca592d0cdb76412608f96d4805b14
Fumiya Sakai <just1factory@gmail.com>
2018-01-10 23:56:31 +0900

    1/10分をとりあえず

--- ReduxSampleSwift/Model/InitialSetting.swift
+++ ReduxSampleSwift/Model/InitialSetting.swift
@@ -11,38 +11,38 @@ import Foundation
 class InitialSetting {
 
     //チュートリアルの終了判定フラグ
-    private static let finishTutorialFlag = "InitialSetting::finishTutorialFlag"
+    private let finishTutorialFlag = "InitialSetting::finishTutorialFlag"
 
     //現在地域の変更設定判定フラグ
-    private static let changeRegionFlag   = "InitialSetting::changeRegionFlag"
+    private let changeRegionFlag   = "InitialSetting::changeRegionFlag"
 
-    //MARK: - Static Function
+    //MARK: - Function
 
     //チュートリアルの終了判定フラグの更新・取得
 
-    static func getFinishTutorialFlag() -> Bool {
+    func getFinishTutorialFlag() -> Bool {
         return ud.bool(forKey: finishTutorialFlag)
     }
 
-    static func setFinishTutorialFlag(result: Bool) {
+    func setFinishTutorialFlag(result: Bool) {
         ud.set(result, forKey: finishTutorialFlag)
         ud.synchronize()
     }
 
     //現在地域の変更設定判定フラグの更新・取得
 
-    static func getChangeRegionFlag() -> Bool {
+    func getChangeRegionFlag() -> Bool {
         return ud.bool(forKey: changeRegionFlag)
     }
 
-    static func setChangeRegionFlag(result: Bool) {
+    func setChangeRegionFlag(result: Bool) {
         ud.set(result, forKey: changeRegionFlag)
         ud.synchronize()
     }
 
     //MARK: - Private Function
 
-    private static var ud: UserDefaults {
+    private var ud: UserDefaults {
         return UserDefaults.standard
     }
 }
