1c5cd0ead3358d89ea0ebdd4459e5447e9688022
Fumiya Sakai <just1factory@gmail.com>
2018-05-05 23:54:05 +0900

    ツールバーボタンの定義

---
+++ ReduxSampleSwift/Module/AppConstants.swift
@@ -0,0 +1,15 @@
+//
+//  AppConstants.swift
+//  ReduxSampleSwift
+//
+//  Created by 酒井文也 on 2018/05/05.
+//  Copyright © 2018年 酒井文也. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct AppConstants {
+    static let FONT_NAME = "HiraKakuProN-W3"
+    static let BOLD_FONT_NAME = "HiraKakuProN-W6"
+}
