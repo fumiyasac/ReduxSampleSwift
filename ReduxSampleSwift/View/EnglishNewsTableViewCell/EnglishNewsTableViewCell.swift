b5daf9fd810fb4576b8bf00ab960d2e073b0b48f
Fumiya Sakai <just1factory@gmail.com>
2018-06-11 21:57:42 +0900

    ニュース表示用のセル作成

---
+++ ReduxSampleSwift/View/EnglishNewsTableViewCell/EnglishNewsTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  EnglishNewsTableViewCell.swift
+//  ReduxSampleSwift
+//
+//  Created by 酒井文也 on 2018/06/09.
+//  Copyright © 2018年 酒井文也. All rights reserved.
+//
+
+import UIKit
+
+class EnglishNewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
