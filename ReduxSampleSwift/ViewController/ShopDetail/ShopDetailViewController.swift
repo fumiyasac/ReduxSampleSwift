3bb942f41cb7842241f77bf691acad59f412cdc4
Fumiya Sakai <just1factory@gmail.com>
2018-07-18 00:45:56 +0900

    サムネイルと詳細表示用Storyboardの紐付け

---
+++ ReduxSampleSwift/ViewController/ShopDetail/ShopDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ShopDetailViewController.swift
+//  ReduxSampleSwift
+//
+//  Created by 酒井文也 on 2018/07/18.
+//  Copyright © 2018年 酒井文也. All rights reserved.
+//
+
+import UIKit
+
+class ShopDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
